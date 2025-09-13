f7647c16efb54ff93cb05dbad279a3e33b5bca2b
FlameCursedSand <157824725+FlameCursedSand@users.noreply.github.com>
2025-09-13 14:32:01 +0300

    Add files via upload

---
+++ Thelab1/Thelab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b, S, P, d, res1, res2;
+        Console.WriteLine("Введите 1 сторону прямоугольника:");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите 2 сторону прямоугольника:");
+        b = Convert.ToDouble(Console.ReadLine());
+        S = a * b;
+        P = 2 * (a + b);
+        d = Math.Sqrt(a*a + b*b);
+        Console.WriteLine($"Площадь = {S}, Периметр = {P}, Диагональ = {d}.");
+    }
+}
